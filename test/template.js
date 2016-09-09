3255c553352a76a009be7b813b849667e17fb338
Matus Szabo <matus.szabo@gmail.com>
2016-09-09 14:37:34 +0200

    added: template replacer

--- test/template.js
+++ test/template.js
@@ -18,7 +18,11 @@ function replace() {
     
     var text = ' asdasd [[asd]] asdad asd asdas asd [[deep.deeparr.0]]';
     var result = template.render(text, testObject);
-    
     assert.strictEqual(result, ' asdasd 23 asdad asd asdas asd asd');
+    
+    text = ' asdasd ${asd} asdad asd asdas asd ${deep.deeparr.0}';
+    result = template.replacer('${','}')(text, testObject);
+    assert.strictEqual(result, ' asdasd 23 asdad asd asdas asd asd');
+    
     console.log('template.render - OK');
 }
